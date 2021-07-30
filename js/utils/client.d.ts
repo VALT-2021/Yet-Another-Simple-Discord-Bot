b29704bd0940f8be2d9c10a37177ee1a80bd6c91
killer069 <65385476+killer069@users.noreply.github.com>
2021-07-30 10:25:15 +0530

    js files deleted

--- js/utils/client.d.ts
+++
@@ -1,8 +0,0 @@
-import Discord from 'discord.js';
-import { SlashCommandHandler, ComponentHandler } from './handler';
-export declare class Client extends Discord.Client {
-    slashCommands: SlashCommandHandler;
-    components: ComponentHandler;
-    constructor(options: Discord.ClientOptions);
-}
-//# sourceMappingURL=client.d.ts.map
