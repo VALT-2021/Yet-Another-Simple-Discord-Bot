bf3224de45a050c57e66917077745087fb14a2a0
killer069 <65385476+killer069@users.noreply.github.com>
2021-07-30 09:24:10 +0530

    testing

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,6 +1,11 @@
 import { Message } from "discord.js";
 import { Client } from "../utils/client";
 
-export function execute(message : Message, client : Client, args : string[]) {
+export = { 
+    "name" : "ping",
+    "cooldown" : 20,
+    execute(message : Message, client : Client, args : string[]) {
     message.channel.send('Command Handler = Said HELLO')
+}
+
 }
--- src/utils/handler.ts
+++ src/utils/handler.ts
@@ -1,12 +1,14 @@
-import { Message } from 'discord.js'
+import { Collection, Message } from 'discord.js'
 import * as fs from 'fs'
 import { Client } from './client'
 
 export class CommandHandler {
-    private __commands: Map<string, string>
-    
+    private __commands: Map<string, any>
+    private __cooldowns : Map<string, Map<string, number>>
+
     constructor(){
         this.__commands = new Map()
+        this.__cooldowns = new Map()
     }
     /**
      * 
@@ -17,7 +19,11 @@ export class CommandHandler {
             var folders = fs.readdirSync(folder)
             if(folders.length !== 0){
                 for(var x in folders){
-                    if(folders[x].endsWith('.js')) this.set(folders[x].replace('.js', ''), `${folder}${folders[x]}`)
+                    if(folders[x].endsWith('.js')) {
+                        const command = require('../' + `${folder}${folders[x]}`)
+                        this.cool_set(command.name)
+                        this.set(command.name, command)
+                    }
                     else continue
                 }
             }
@@ -26,8 +32,8 @@ export class CommandHandler {
         else console.log(`Folder is not found : ${folder}`)
     }
 
-    private set(command_name : string, foldername : string){
-        this.__commands.set(command_name.toLowerCase(), foldername)
+    private set(command_name : string, exe_command : any){
+        this.__commands.set(command_name.toLowerCase(), exe_command)
         return this.__commands
     }
 
@@ -35,6 +41,34 @@ export class CommandHandler {
         return this.__commands.get(command_name.toLowerCase())
     }
 
+    private cool_set(command_name : string){
+        this.__cooldowns.set(command_name, new Map())
+        return this.__cooldowns
+    }
+
+    private cool_get(command_name : string){
+        return this.__cooldowns.get(command_name)
+    }
+
+    private cool_start(command_name:string, timestamp : number ,author_id : string): boolean{
+        console.log(`Author ID : ${author_id}\nTimestamp : ${timestamp}`)
+        this.__cooldowns.get(command_name)?.set(author_id, timestamp)
+        console.log('')
+        console.log(this.__cooldowns)
+        if(this.cool_get(command_name)) return false
+        else return true
+    }
+
+    private cool_end(command_name:string, author_id : string): boolean{
+        this.cool_get(command_name)?.delete(author_id)
+        if(this.cool_get(command_name)) return false
+        else return true
+    }
+
+    private cool_status(command_name:string, author_id:string){
+        if(this.cool_get(command_name)?.has(author_id)) return true
+        else return false
+    }
     /**
      * 
      * @param command_name Command you want to run
@@ -43,9 +77,16 @@ export class CommandHandler {
      * @param args Rest arguements of message
      */
     run(command_name:string, message : Message , client : Client, args : string[]): void{
-        var folder = this.get(command_name) as string
-        const { execute } = require('../' + folder)
-        execute(message, client, args);
+        var cooldown_sec = this.get(command_name).cooldown
+        var time = Date.now() + cooldown_sec * 1000
+        if(!this.cool_status(command_name, message.author.id)){
+            this.cool_start(command_name, time, message.author.id)
+            this.get(command_name).execute(message, client, args);
+            setTimeout(() => {
+                this.cool_end(command_name, message.author.id)
+            }, cooldown_sec)
+        }
+        else message.channel.send(`You need to wait for sometime before running ${command_name} command.`)
     }
 
     /**
